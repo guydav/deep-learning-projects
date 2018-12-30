b97da839d40ec3c0d3c52bcc7d02392f73714512
Guy Davidson <guydav@gmail.com>
2018-12-30 14:35:55 +0200

    Last fixes

--- projects/vae_abc/vae.py
+++ projects/vae_abc/vae.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image

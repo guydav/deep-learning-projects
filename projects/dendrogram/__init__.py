851f38c884e0e4613fdb0011d88166b7d1aeedc6
Guy Davidson <guydav@gmail.com>
2019-01-02 00:50:13 +0200

    Continuing to debug the __init__ files

--- projects/dendrogram/__init__.py
+++ projects/dendrogram/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [
-    '..metalearning.base_model',
-    '..metalearning.cnn_mlp',
+    # '..metalearning.base_model',
+    # '..metalearning.cnn_mlp',
     'cifar10',
     'cnn_model',
     'dendrogram',
@@ -8,8 +8,8 @@ __all__ = [
     'tests_and_plots'
 ]
 
-from ..metalearning import base_model
-from ..metalearning import cnnmlp
+# from ..metalearning import base_model
+# from ..metalearning import cnnmlp
 from . import cifar10
 from . import cnn_model
 from . import dendrogram

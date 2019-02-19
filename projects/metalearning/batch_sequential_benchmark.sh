657011ebcd098cb2d01bceaaa5fca52a3d681c0b
Guy Davidson <guydav@gmail.com>
2019-02-19 15:20:53 -0800

    And now added support to pass additional arguments to the shell script.

--- projects/metalearning/batch_sequential_benchmark.sh
+++ projects/metalearning/batch_sequential_benchmark.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ $# < 3 || $# > 4 ]];  then
-    echo "Usage: ./batch_sequential_benchmark.sh <GPU ID> <Number of repetitions> <Dataset Random seed> [optional train coreset size]"
+if [[ $# < 3 ]];  then
+    echo "Usage: ./batch_sequential_benchmark.sh <GPU ID> <Number of repetitions> <Dataset Random seed> [additional arguments to the internal script]"
     exit 1
 fi
 
 gpu_id=$1
 num_reps=$2
 initial_dataset_random_seed=$3
-
-if [[ $# == 4 ]]; then
-    coreset_size=$4
-else
-    coreset_size=22500
-fi
 
 export CUDA_DEVICE_ORDER='PCI_BUS_ID'
 export CUDA_VISIBLE_DEVICES=${gpu_id}
@@ -23,7 +17,7 @@ wandb login 9676e3cc95066e4865586082971f2653245f09b4
 for i in `seq 1 ${num_reps}`; do
     # TODO: how do I specify boolean args? Integer?
     let "current_random_seed = ${initial_dataset_random_seed} + ${i}"
-    python run_sequential_benchmark.py --train_coreset_size ${coreset_size} --dataset_random_seed ${current_random_seed}
+    python run_sequential_benchmark.py --dataset_random_seed ${current_random_seed} ${@:4}
     sleep 1s;
 done
 

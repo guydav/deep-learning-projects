9ecb8c38cee03380ca24e03b59b57ead5de0ae83
Guy Davidson <guydav@gmail.com>
2019-02-19 16:12:11 -0800

    Debugging

--- projects/metalearning/batch_sequential_benchmark.sh
+++ projects/metalearning/batch_sequential_benchmark.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo $#
+echo $@
+
 if [[ $# < 4 ]];  then
     echo "Usage: ./batch_sequential_benchmark.sh <GPU ID> <Number of repetitions> <run id> <Dataset Random seed> [additional arguments to the internal script]"
     exit 1
@@ -18,7 +21,7 @@ wandb login 9676e3cc95066e4865586082971f2653245f09b4
 for i in `seq 1 ${num_reps}`; do
     # TODO: how do I specify boolean args? Integer?
     let "current_random_seed = ${initial_dataset_random_seed} + ${i}"
-    python run_sequential_benchmark.py --id ${run_id} --dataset_random_seed ${current_random_seed} ${@:5}
+    echo "python run_sequential_benchmark.py --id ${run_id} --dataset_random_seed ${current_random_seed} ${@:5}"
     sleep 1s;
 done
 

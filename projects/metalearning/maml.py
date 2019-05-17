9911b82ca9f47cc5e2aef17f9997e6d1022a17d7
Guy Davidson <guydav@gmail.com>
2019-05-17 10:22:20 -0700

    Debugging the weight saving bit.

--- projects/metalearning/maml.py
+++ projects/metalearning/maml.py
@@ -47,7 +47,7 @@ class MamlModel(BasicModel):
         _, meta_train_task_indices = Q_meta_train.max(1)
 
         pre_training_weights = copy.deepcopy(self.state_dict())
-        print(pre_training_weights.keys())
+        if debug: print(pre_training_weights['fcout.fc5.weight'].shape)
         need_load = False
 
         for task in active_tasks:

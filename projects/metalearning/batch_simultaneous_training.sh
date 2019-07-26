2b22e1702a8c02dda50d8ae898aecbc7dcbbe638
Guy Davidson <guydav@gmail.com>
2019-07-26 12:41:16 +0100

    Used the wrong (read: outdated) bash script as the baseline.

--- projects/metalearning/batch_simultaneous_training.sh
+++ projects/metalearning/batch_simultaneous_training.sh
@@ -11,20 +11,22 @@
 #fi
 
 gpu_id=$1
-num_reps=$2
-run_name=$3
-initial_dataset_random_seed=$4
+start_index=$2
+let "end_index = $3 - 1"
+run_name=$4
+random_seed=$5
 
 export CUDA_DEVICE_ORDER='PCI_BUS_ID'
 export CUDA_VISIBLE_DEVICES=${gpu_id}
 
 wandb login 9676e3cc95066e4865586082971f2653245f09b4
 
-for i in `seq 1 ${num_reps}`; do
-    let "current_random_seed = ${initial_dataset_random_seed} + ${i}"
-    python run_simultaneous_training.py --name ${run_name} --dataset_random_seed ${current_random_seed} ${@:5}
+for i in `seq ${start_index} ${end_index}`; do
+    let "current_random_seed = ${random_seed} + ${i}"
+    python run_simultaneous_training.py --name ${run_name} --dataset_random_seed ${current_random_seed} ${@:6}
     sleep 1s;
 done
+
 
 
 
